9ffb97ed755d49c9610f285ecfea8e2744f99fb1
Mouha747 <mouhamedsow970@gmail.com>
2023-09-11 17:56:31 +0200

    creation de la menu

---
+++ fiche1.py
@@ -0,0 +1,16 @@
+un = "mafe"
+deux = "thiebou dieune"
+trois = "domada" 
+print("Bonjour et bienvenue dans notre Restaurant!")
+print("Voici nos menus:")
+print("1: mafe \n2: thiebou dieune \n3: domada ")
+a = input("veuillez choisir votre plat :")
+if a == "un":
+    print("vous avez choisi le mafe")
+elif a == "deux":
+    print("vous avez choisi le thiebou dieune")
+elif a == "trois":
+    print("vous avez choisi le domada")
+else:
+    print("choix non valide")
+print("Au revoir!!")
